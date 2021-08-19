b6e4189e6ba87e77790f2591facd0e5cedf0eca9
DeveloprAbhishek <developr.abhishek@gmail.com>
2021-08-19 17:22:00 +0530

    Kotlin Scoping Functions Initial commit

---
+++ _Kotlin Projects/Kotlin_Scoping_Functions_IW_19Aug21/app/src/main/java/com/example/kotlin_scoping_functions_iw_19aug21/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin_scoping_functions_iw_19aug21
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
