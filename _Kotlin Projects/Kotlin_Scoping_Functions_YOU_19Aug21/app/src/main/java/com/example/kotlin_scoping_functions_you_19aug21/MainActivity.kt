97e88696500427b2e1816cbc390bd58b3e4d2da6
DeveloprAbhishek <developr.abhishek@gmail.com>
2021-08-19 23:30:38 +0530

    initial commit

---
+++ _Kotlin Projects/Kotlin_Scoping_Functions_YOU_19Aug21/app/src/main/java/com/example/kotlin_scoping_functions_you_19aug21/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin_scoping_functions_you_19aug21
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
