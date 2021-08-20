4be3fd8cb3c887282b501cd5912717f0fa6050a7
DeveloprAbhishek <developr.abhishek@gmail.com>
2021-08-20 14:50:11 +0530

    added design

---
+++ _Kotlin Projects/Navigation_Graph_IW_20Aug21/app/src/main/java/com/example/navigation_graph_iw_20aug21/StudentPerformanceDetailsFragment.kt
@@ -0,0 +1,10 @@
+package com.example.navigation_graph_iw_20aug21
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class StudentPerformanceDetailsFragment : Fragment(R.layout.fragment_student_performance_details) {
+}
